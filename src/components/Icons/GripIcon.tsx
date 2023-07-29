68e2895f23daaad495640c7a8423508e64705f40
LilyStilson <lunamusicoff@gmail.com>
2023-07-30 01:29:45 +0200

    Fixed GripIcon

--- src/components/Icons/GripIcon.tsx
+++ src/components/Icons/GripIcon.tsx
@@ -4,12 +4,12 @@ import IconBase from "./IconBase";
 export default function GripIcon(props: IDefaultIconsProps) {
     return (
         <IconBase {...props} viewBox="0 0 24 24">
-            <circle cx="9" cy="5"></circle>
-            <circle cx="9" cy="12"></circle>
-            <circle cx="9" cy="19"></circle>
-            <circle cx="15" cy="5"></circle>
-            <circle cx="15" cy="12"></circle>
-            <circle cx="15" cy="19"></circle>
+            <circle r="2" cx="9" cy="5"></circle>
+            <circle r="2" cx="9" cy="12"></circle>
+            <circle r="2" cx="9" cy="19"></circle>
+            <circle r="2" cx="15" cy="5"></circle>
+            <circle r="2" cx="15" cy="12"></circle>
+            <circle r="2" cx="15" cy="19"></circle>
         </IconBase>
     )
 }
